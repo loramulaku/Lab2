02bd1cbd7064999b674a4c184c6541da579ffb4b
RRAFSHI <arianitrrafshi1@gmail.com>
2026-05-01 18:12:10 +0200

    Candidate/Recruiter id!=null

--- backend/src/models/sql/RecruiterProfile.js
+++ backend/src/models/sql/RecruiterProfile.js
@@ -2,10 +2,9 @@ const { DataTypes } = require('sequelize');
 const { sequelize } = require('../../config/mysql');
 
 const RecruiterProfile = sequelize.define('RecruiterProfile', {
-  id:        { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-  userId:    { type: DataTypes.INTEGER, unique: true, field: 'user_id' },
-  companyId: { type: DataTypes.INTEGER, field: 'company_id' },
-}, {
+  id:     { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  userId: { type: DataTypes.INTEGER, allowNull: false,unique: true, field: 'user_id' },
+  companyId: { type: DataTypes.INTEGER, allowNull: false,field: 'company_id' }, 
   tableName: 'RecruiterProfiles',
   timestamps: false,
 });
